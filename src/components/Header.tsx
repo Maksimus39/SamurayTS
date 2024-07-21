984a9cc57d8bc9d9b8f988af0711c83fe08e7471
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-21 23:28:51 +0300

    Новая компонент Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://seousluga.ru/files/images/izobrazhenie_2024-02-07_143427682.png" alt=""/>
+        </header>
+    )
+}
