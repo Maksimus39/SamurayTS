7c7182813c51571ea5d4a558afc866564a2ae722
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-21 23:29:16 +0300

    Новая компонент Profile

---
+++ src/components/Profille.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://www.funnyart.club/uploads/posts/2022-05/1653886640_47-www-funnyart-club-p-kartinki-sotsialnikh-setei-krasivo-56.png"
+                    alt=""/>
+            </div>
+
+            <div>
+                ava + description
+            </div>
+
+            <div>
+                My Posts
+                <div>
+                    New Post
+                </div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+
+        </div>
+    )
+}
