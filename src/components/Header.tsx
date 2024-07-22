9b2e8c79389a95d21b2e374f5a81168f36536276
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-22 11:34:38 +0300

    Проимпортировал уникальные классы стилей

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from './Header.module.css'
 
 export const Header = () => {
     return (
-        <header className='header'>
+        <header className={classes.header}>
             <img src="https://seousluga.ru/files/images/izobrazhenie_2024-02-07_143427682.png" alt=""/>
         </header>
     )
