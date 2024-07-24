2f57e7c52eb37630b9616132b2f066c7ea6e7917
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-24 17:02:27 +0300

    Протипизировал данные и отрисовал их

--- src/components/profile/myPosts/posts/Post.tsx
+++ src/components/profile/myPosts/posts/Post.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import classes from "./Post.module.css"
 
-export const Post = () => {
+type PostProps = {
+    message: string
+}
+export const Post = (props: PostProps) => {
     return (
         <div className={classes.item}>
             <img
                 src="https://sneg.top/uploads/posts/2023-06/1687597602_sneg-top-p-ava-obshchenie-instagram-1.jpg"
                 alt=""/>
-            <span>Post 1</span>
+            <span>{props.message}</span>
             <div>
                 <span>Like</span>
             </div>
