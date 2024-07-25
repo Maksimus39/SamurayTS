d683ff33e7c10c9df5f585606361d28334998145
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-25 23:41:57 +0300

    вынес стили на уровень выше

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -4,7 +4,7 @@ import {MyPosts} from "./myPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
                 <img
                     src="https://avatars.dzeninfra.ru/get-zen_doc/271828/pub_655658c72ac2da7753ee753c_6556596de3bfcb307785efa6/scale_1200"
