7862c1f4e5c71d0fbdb7a7f8384897085896fc29
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-26 00:28:09 +0300

    Обернул компоненты в BrowserRouter и прописал пути для каждой отдельной компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,29 @@ import {Header} from "./components/header/Header";
 import {Navbar} from "./components/navbar/Navbar";
 import {Profile} from "./components/profile/Profile";
 import {Dialogs} from "./components/dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import {News} from "./components/news/News";
+import {Music} from "./components/music/Music";
+import {Settings} from "./components/settings/Settings";
 
 
 function App() {
     return (
-        <div className={'app-wrapper'}>
+        <BrowserRouter>
+            <div className={'app-wrapper'}>
 
-            <Header/>
-            <Navbar/>
+                <Header/>
+                <Navbar/>
 
-            <div className={'app-wrapper-content'}>
-                {/*<Profile/>*/}
-                <Dialogs/>
+                <div className={'app-wrapper-content'}>
+                    <Route path={'/profile'} component={Profile}/>
+                    <Route path={'/dialogs'} component={Dialogs}/>
+                    <Route path={'/news'} component={News}/>
+                    <Route path={'/music'} component={Music}/>
+                    <Route path={'/settings'} component={Settings}/>
+                </div>
             </div>
-
-        </div>
+        </BrowserRouter>
     );
 }
 
