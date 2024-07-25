9eb33b94df3d620a9e1e1592104608b3af4474a0
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-25 23:40:32 +0300

    Новая компонент и общий стиль

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {Header} from "./components/header/Header";
 import {Navbar} from "./components/navbar/Navbar";
 import {Profile} from "./components/profile/Profile";
+import {Dialogs} from "./components/dialogs/Dialogs";
+
 
 function App() {
     return (
@@ -10,7 +12,12 @@ function App() {
 
             <Header/>
             <Navbar/>
-            <Profile/>
+
+            <div className={'app-wrapper-content'}>
+                {/*<Profile/>*/}
+                <Dialogs/>
+            </div>
+
         </div>
     );
 }
