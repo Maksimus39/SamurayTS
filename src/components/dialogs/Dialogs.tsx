89a17af16cb3cf91f3bdd1ac0e75d758d0abcf75
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-26 10:52:37 +0300

    Новая  компонент Dialogs

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,8 +3,27 @@ import classes from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Macsim
+                </div>
+                <div className={classes.dialog}>
+                    Larisa
+                </div>
+                <div className={classes.dialog}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Bogdan
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Привет, как твои дела?</div>
+                <div className={classes.message}>Привет, ты будеш ужинать!</div>
+                <div className={classes.message}>Мам, можно я пойду погуляю с друзьями?</div>
+                <div className={classes.message}>Пап, помоги мне с домашним заданием по математике.</div>
+            </div>
         </div>
     );
 };
