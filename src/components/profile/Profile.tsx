5e1deccbc30572ce08248917a6ddd10d5761e756
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-27 19:13:43 +0300

    Вынесли данные в отдельную компонент ProfileInfo

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,20 +1,12 @@
 import React from "react";
-import classes from './Profile.module.css'
 import {MyPosts} from "./myPosts/MyPosts";
+import {ProfileInfo} from "./profileInfo/ProfileInfo";
+import classes from './Profile.module.css'
 
 export const Profile = () => {
     return (
         <div>
-            <div>
-                <img
-                    src="https://avatars.dzeninfra.ru/get-zen_doc/271828/pub_655658c72ac2da7753ee753c_6556596de3bfcb307785efa6/scale_1200"
-                    alt=""/>
-            </div>
-
-            <div>
-                ava + description
-            </div>
-
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     )
