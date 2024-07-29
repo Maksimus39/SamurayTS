1fa13b30d0f5cad4a71686f952bce9cbfc780992
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-29 23:33:44 +0300

    Вынес данные и передал их в типизации

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import classes from "./MyPosts.module.css"
 import {Post} from "./posts/Post";
+import {PostDataType} from "../Profile";
 
-export const MyPosts = () => {
 
-    let postData = [
-        {id: 1, message: 'Hi how are you', likesCount: 15},
-        {id: 2, message: 'it`s my first post', likesCount: 20},
-    ]
 
-    let postDataElement = postData.map((postData)=>{
+type MyPostType={
+     postData:PostDataType[]
+}
+export const MyPosts = (props:MyPostType) => {
+
+    let postDataElement = props.postData.map((postData) => {
         return (
             <Post message={postData.message} likesCount={postData.likesCount}/>
         )
