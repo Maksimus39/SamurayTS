07a7f20db02ba0ccfe19e7f269126cf59ad1f2cf
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-29 23:33:57 +0300

    Вынес данные и передал их в типизации

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -3,11 +3,19 @@ import {MyPosts} from "./myPosts/MyPosts";
 import {ProfileInfo} from "./profileInfo/ProfileInfo";
 import classes from './Profile.module.css'
 
-export const Profile = () => {
+export type PostDataType = {
+    id: number
+    message: string
+    likesCount: number
+}
+type ProfileType = {
+    postData: PostDataType[]
+}
+export const Profile = (props: ProfileType) => {
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts postData={props.postData}/>
         </div>
     )
 }
