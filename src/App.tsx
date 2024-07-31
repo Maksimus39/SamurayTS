efa169badf84e73c0c362b1801ac5f22bad1b580
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-31 19:32:04 +0300

    Передаю стейт в дочерние компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,27 @@ import {BrowserRouter, Route} from "react-router-dom";
 import {News} from "./components/news/News";
 import {Music} from "./components/music/Music";
 import {Settings} from "./components/settings/Settings";
-import {DialogDataType, MessagesDataType, PostDataType} from "./index";
 import {Profile} from "./components/profile/Profile";
+import {RootStateType} from "./components/redux/state";
 
 type AppProps = {
-    dialogs: DialogDataType[]
-    messages: MessagesDataType[]
-    posts: PostDataType[]
+    state: RootStateType
 }
 
 function App(props: AppProps) {
-
 
     return (
         <BrowserRouter>
             <div className={'app-wrapper'}>
 
                 <Header/>
-                <Navbar/>
+                <Navbar friends={props.state.sidebar.friends}/>
 
                 <div className={'app-wrapper-content'}>
-                    <Route path={'/profile'} render={() => <Profile posts={props.posts}/>}/>
+                    <Route path={'/profile'} render={() => <Profile posts={props.state.messagesPage.posts}/>}/>
                     <Route path={'/dialogs'}
-                           render={() => <Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
+                           render={() => <Dialogs dialogs={props.state.profilePage.dialogs}
+                                                  messages={props.state.profilePage.messages}/>}/>
                     <Route path={'/news'} component={News}/>
                     <Route path={'/music'} component={Music}/>
                     <Route path={'/settings'} component={Settings}/>
