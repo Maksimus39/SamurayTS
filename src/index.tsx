e3bf3f0fcef77d751b8dbf235c94b6a26181a23a
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-31 19:34:49 +0300

    вынес стейт на уровень выше и потом просто передал его в качестве аргумента

--- src/index.tsx
+++ src/index.tsx
@@ -2,44 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-export type DialogDataType = {
-    id: string
-    name: string
-}
-export type MessagesDataType = {
-    message: string
-}
-export type PostDataType = {
-    id: number
-    message: string
-    likesCount: number
-}
-
-
-let dialogs: DialogDataType[] = [
-    {id: "1", name: 'Macsim'},
-    {id: "2", name: 'Larisa'},
-    {id: "3", name: 'Andrey'},
-    {id: "4", name: 'Bogdan'},
-]
-let messages: MessagesDataType[] = [
-    {message: 'Привет, как твои дела?'},
-    {message: 'Привет, ты будешь ужинать!'},
-    {message: 'Мам, можно я пойду погуляю с друзьями?'},
-    {message: 'Пап, помоги мне с домашним заданием по математике.'},
-]
-let posts: PostDataType[] = [
-    {id: 1, message: 'Hi how are you', likesCount: 15},
-    {id: 2, message: 'it`s my first post', likesCount: 20},
-]
+import {state} from "./components/redux/state"
 
 
 ReactDOM.render(
-    <App
-        dialogs={dialogs}
-        messages={messages}
-        posts={posts}
-    />,
-    document.getElementById('root')
+    <App state={state}/>, document.getElementById('root')
 );
