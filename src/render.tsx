88a7e74d93a7e24b394ca8c1ff8f2e8e87212e74
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-06 23:40:45 +0300

    Передал колбек в дочернюю компоненту

--- src/render.tsx
+++ src/render.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, RootStateType} from "./components/redux/state"
+import {addPost, RootStateType, updateNewPostText} from "./components/redux/state"
 import {BrowserRouter} from "react-router-dom";
 
-export let rerenderEntireThree = (state:RootStateType) => {
+export let rerenderEntireThree = (state: RootStateType) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost}/>
+            <App state={state}
+                 addPost={addPost}
+                 updateNewPostText={updateNewPostText}
+            />
         </BrowserRouter>
         , document.getElementById('root')
     );
