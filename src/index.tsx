03d68be70e6063e27546569deb4480290179f786
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-07 09:13:32 +0300

    Перенос логики из рендер в индекс

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import './index.css';
-import {rerenderEntireThree} from "./render";
-import {state} from "./components/redux/state";
+import {addPost, RootStateType, state, subscribe, updateNewPostText} from "./components/redux/state";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+import React from "react";
 
+
+export let rerenderEntireThree = (state: RootStateType) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state}
+                 addPost={addPost}
+                 updateNewPostText={updateNewPostText}
+            />
+        </BrowserRouter>
+        , document.getElementById('root')
+    );
+}
 
 rerenderEntireThree(state);
+
+
+subscribe(() => rerenderEntireThree(state))
+
