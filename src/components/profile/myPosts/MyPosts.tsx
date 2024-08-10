08fdd74563392fed2f1f08662f6f665a52c24d4c
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-10 23:12:59 +0300

    В методе dispatch вызвали экшен креэйторы

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -1,15 +1,15 @@
 import React, {useRef} from 'react';
 import classes from "./MyPosts.module.css"
 import {Post} from "./posts/Post";
-import {DispatchType, PostDataType} from "../../redux/state";
+import {addPostActionCreator, DispatchType, PostDataType, updateNewPostTextActionCreator} from "../../redux/state";
 
 
 type MyPostType = {
     posts: PostDataType[]
     newPostText: string
     dispatch: (action: DispatchType) => void
-
 }
+
 export const MyPosts = (props: MyPostType) => {
 
     let postDataElement = props.posts.map((p) => {
@@ -22,14 +22,17 @@ export const MyPosts = (props: MyPostType) => {
     const newPostElement = useRef<HTMLTextAreaElement>(null);
 
     const addPost = () => {
-        props.dispatch({type: 'ADD-POST', newPostText: ''})
+        if (newPostElement.current) {
+            const text = newPostElement.current.value;
+            props.dispatch(addPostActionCreator(text))
+        }
     };
 
     const onPostChange = () => {
         if (newPostElement.current) {
             const text = newPostElement.current.value;
             if (text.trim() !== "") {
-                props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text.trim()})
+                props.dispatch(updateNewPostTextActionCreator(text.trim()))
             }
         }
     }
