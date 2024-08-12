93058a449da907e43092e878edefe7f3aa836b02
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-12 17:41:15 +0300

    Поменял импорты данных

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from './Navbar.module.css';
 import {NavLink} from "react-router-dom";
-import {FriendsDataType} from "../redux/state";
+import {FriendsDataType} from "../redux/store";
 import {NavbarFriends} from "./navbarFriends/NavbarFriends";
 
 
