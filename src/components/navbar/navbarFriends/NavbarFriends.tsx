ce9b84bf00253ee2b0e4da0c76985132ed1d231d
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-15 00:09:07 +0300

    Компонент NavbarFriends заиспользовал StoreContext.Consumer

--- src/components/navbar/navbarFriends/NavbarFriends.tsx
+++ src/components/navbar/navbarFriends/NavbarFriends.tsx
@@ -1,24 +1,27 @@
-import {FriendsDataType} from "../../redux/store";
 import classes from "../Navbar.module.css";
 import React from "react";
+import {StoreContext} from "../../StoreContext";
 
-type NavbarFriendsProps = {
-    friends: FriendsDataType[]
-}
-export const NavbarFriends = (props: NavbarFriendsProps) => {
+export const NavbarFriends = () => {
     return (
-
-        <div>
-            {props.friends.map((fr) => {
+        <StoreContext.Consumer>
+            {store => {
+                const friends = store.getState().sidebarPage.friends; // Предполагается, что friends находятся в store.getState()
                 return (
-                    <div className={classes.description}>
-                        <div className={classes.text}>
-                            {fr.name}
-                        </div>
-                        <img src={fr.img} alt={fr.name}/>
+                    <div>
+                        {friends.map((fr) => {
+                            return (
+                                <div key={fr.id} className={classes.description}>
+                                    <div className={classes.text}>
+                                        {fr.name}
+                                    </div>
+                                    <img src={fr.img} alt={fr.name}/>
+                                </div>
+                            );
+                        })}
                     </div>
-                )
-            })}
-        </div>
-    )
-}
+                );
+            }}
+        </StoreContext.Consumer>
+    );
+};
