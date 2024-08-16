c560c38a78e89f9750aa7a6673afb4d3e7a8fc14
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-16 23:09:38 +0300

    Рефакторинг DialogsContainer и переписал под библиотеку реакт-редакс

--- src/components/dialogs/DialogsContainer.tsx
+++ src/components/dialogs/DialogsContainer.tsx
@@ -1,32 +1,23 @@
-import React from 'react';
+import {AppRootStateType} from "../redux/redux-store";
 import {addNewMessageTextActionCreator, updateNewMessageTextActionCreator} from "../redux/reducer/dialogsPageReducer";
+import {connect} from "react-redux";
 import {Dialogs} from "./Dialogs";
-import {StoreContext} from "../StoreContext";
 
+let mapStateToProps = (state: AppRootStateType) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
+}
 
-export const DialogsContainer = () => {
-    return (
-        <StoreContext.Consumer>
-            {
-                (store) => {
+let mapDispatchToProps = (dispatch: any) => {
+    return {
+        updateNewMessageBody: (message: string) => {
+            dispatch(addNewMessageTextActionCreator(message));
+        },
+        sendMessage: (message: string) => {
+            dispatch(updateNewMessageTextActionCreator(message))
+        },
+    }
+}
 
-                    let state = store.getState().dialogsPage;
-
-                    const addMessageHandler = () => {
-                        const message = state.newMessageText;
-                        store.dispatch(addNewMessageTextActionCreator(message));
-                    }
-
-                    const onMessageChange = (message: string) => {
-                        store.dispatch(updateNewMessageTextActionCreator(message))
-                    }
-                    return <Dialogs store={store}
-                                    updateNewMessageBody={onMessageChange}
-                                    sendMessage={addMessageHandler}
-                                    dialogsPage={state}
-                    />
-                }}
-        </StoreContext.Consumer>
-    )
-};
-
+export const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs)
