223fef72aaca70d024e51bb3fd351c7cdcc7a15c
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-20 23:09:08 +0300

    Добавил новый редюсер в store

--- src/components/redux/redux-store.ts
+++ src/components/redux/redux-store.ts
@@ -2,12 +2,14 @@ import {combineReducers, createStore, Store} from "redux";
 import {profilePageReducer} from "./reducer/profilePageReducer";
 import {dialogsPageReducer} from "./reducer/dialogsPageReducer";
 import {sidebarPageReducer} from "./reducer/SidePageReducer";
+import {usersPageReducer} from "./reducer/usersPageReducer";
 
 
 let reducer = combineReducers({
     profilePage: profilePageReducer,
     dialogsPage: dialogsPageReducer,
     sidebarPage: sidebarPageReducer,
+    usersPage: usersPageReducer
 })
 
 export type AppRootStateType = ReturnType<typeof reducer>
