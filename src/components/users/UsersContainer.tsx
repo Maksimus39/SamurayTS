abcafba461c5b98a2e208dd9f40980ef5eb21801
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-24 19:47:58 +0300

    Добавление коллбеком и их описание типизации

--- src/components/users/UsersContainer.tsx
+++ src/components/users/UsersContainer.tsx
@@ -2,23 +2,36 @@ import {connect} from "react-redux";
 import {AppRootStateType} from "../redux/redux-store";
 import {Dispatch} from "redux";
 import {UsersDataType} from "../redux/store";
-import {followActionCreator, setUsersActionCreator, unfollowActionCreator} from "../redux/reducer/usersPageReducer";
-import {UsersC} from "./UsersС";
+import {
+    followActionCreator,
+    setCurrentPageActionCreator,
+    setUsersActionCreator, setUsersTotalCountActionCreator,
+    unfollowActionCreator
+} from "../redux/reducer/usersPageReducer";
+import {Users} from "./Users";
 
 
 export type MapStateToProps = {
     users: UsersDataType[]
+    pageSize: number
+    totalUsersCount: number
+    currentPage: number
 }
 export type MapDispatchToProps = {
     follow: (userId: number) => void
     unfollow: (userId: number) => void
     setUsers: (users: UsersDataType[]) => void
+    setCurrentPage: (pageNumber: number) => void
+    setTotalUsersCount: (totalCount: number) => void
 }
 export type DialogsUsersPropsType = MapStateToProps & MapDispatchToProps;
 
 let mapStateToProps = (state: AppRootStateType): MapStateToProps => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
     }
 }
 let mapDispatchToProps = (dispatch: Dispatch): MapDispatchToProps => {
@@ -31,8 +44,14 @@ let mapDispatchToProps = (dispatch: Dispatch): MapDispatchToProps => {
         },
         setUsers: (users: UsersDataType[]) => {
             dispatch(setUsersActionCreator(users))
+        },
+        setCurrentPage: (pageNumber: number) => {
+            dispatch(setCurrentPageActionCreator(pageNumber))
+        },
+        setTotalUsersCount: (totalCount: number) => {
+            dispatch(setUsersTotalCountActionCreator(totalCount))
         }
     }
 }
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersC)
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
