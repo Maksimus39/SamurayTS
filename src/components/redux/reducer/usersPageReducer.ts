078d73abeb7b6e485cf3fc6712c5a7ec023c08b6
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-24 19:49:07 +0300

    Описание двух ActionType и двух ActionCreator с их обработкой в редюсере

--- src/components/redux/reducer/usersPageReducer.ts
+++ src/components/redux/reducer/usersPageReducer.ts
@@ -12,6 +12,15 @@ export type SerUsersActionType = {
     type: 'SET-USERS'
     users: UsersDataType[]
 }
+export type SetCurrentPageActionType = {
+    type: 'SET-CURRENT-PAGE'
+    currentPage: number
+}
+export type SetUsersTotalCountActionType = {
+    type: 'SET-USERS-COUNT'
+    totalUsersCount: number
+}
+
 export const followActionCreator = (userId: number): FollowActionType => {
     return {
         type: 'FOLLOW',
@@ -30,12 +39,25 @@ export const setUsersActionCreator = (users: UsersDataType[]): SerUsersActionTyp
         users: users
     } as const
 }
-
-const initialState: UsersPageType = {
-    users: []
+export const setCurrentPageActionCreator = (currentPage: number): SetCurrentPageActionType => {
+    return {
+        type: 'SET-CURRENT-PAGE',
+        currentPage: currentPage
+    } as const
+}
+export const setUsersTotalCountActionCreator = (totalUsersCount: number): SetUsersTotalCountActionType => {
+    return {
+        type: 'SET-USERS-COUNT',
+        totalUsersCount: totalUsersCount
+    } as const
 }
 
-
+const initialState: UsersPageType = {
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1
+}
 
 
 export const usersPageReducer = (state: UsersPageType = initialState, action: DispatchType): UsersPageType => {
@@ -45,21 +67,28 @@ export const usersPageReducer = (state: UsersPageType = initialState, action: Di
             return {
                 ...state,
                 users: state.users.map(u =>
-                    u.id === action.userId ? { ...u, followed: true } : u
+                    u.id === action.userId ? {...u, followed: true} : u
                 )
             };
         case "UNFOLLOW":
             return {
                 ...state,
                 users: state.users.map(u =>
-                    u.id === action.userId ? { ...u, followed: false } : u
+                    u.id === action.userId ? {...u, followed: false} : u
                 )
             };
         case "SET-USERS":
             return {
-                ...state,
-                users: [...state.users, ...action.users]
+                ...state, users: action.users
             };
+        case "SET-CURRENT-PAGE":
+            return {
+                ...state, currentPage: action.currentPage
+            }
+        case "SET-USERS-COUNT":
+            return {
+                ...state,totalUsersCount: action.totalUsersCount
+            }
         default:
             return state;
     }
