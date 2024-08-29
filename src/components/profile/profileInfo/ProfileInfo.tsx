bd932ae3a52e2e0a3acf067ed6ceaffac4687e40
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-29 12:24:21 +0300

    отрисовка данных из типизации

--- src/components/profile/profileInfo/ProfileInfo.tsx
+++ src/components/profile/profileInfo/ProfileInfo.tsx
@@ -1,30 +1,46 @@
 import React from "react";
-import classes from './ProfileInfo.module.css'
 import {Preloader} from "../../common/preloader/Preloader";
+import {ContactsType, ProfileType} from "../Profile";
 
 type ProfileInfoProps = {
-    profile: {
-        photos:{
-            large:string
-        }
-    }
+    profile: ProfileType;
 }
+
+const Contacts = ({ contacts }: { contacts: ContactsType }) => (
+    <div>
+        <h3>Мои контакты!</h3>
+        {Object.entries(contacts).map(([key, value]) => (
+            value ? <div key={key}>{key}: {value}</div> : null
+        ))}
+    </div>
+);
+
 export const ProfileInfo = (props: ProfileInfoProps) => {
     if (!props.profile) {
-        return <Preloader/>
+        return <Preloader />;
     }
+
+    const { profile } = props;
+
     return (
-        <div>
+        <React.Fragment>
             <div>
                 <img
                     src="https://avatars.dzeninfra.ru/get-zen_doc/271828/pub_655658c72ac2da7753ee753c_6556596de3bfcb307785efa6/scale_1200"
-                    alt=""/>
+                    alt="Profile background"
+                />
             </div>
 
-            <div className={classes.descriptionBlock}>
-                <img src={props.profile.photos.large} alt=""/>
-                ava + description
+            <div>
+                <img src={profile.photos.small} alt="Profile avatar"/>
+                <div>
+                    <h2>Обо мне: {profile.aboutMe}</h2>
+                    <Contacts contacts={profile.contacts} />
+                    <h2>Моё имя: {profile.fullName}</h2>
+                    <h2>Место работы: {profile.lookingForAJob ? 'Ищу работу' : 'Не ищу работу'}</h2>
+                    <h2>Вакансии: {profile.lookingForAJobDescription}</h2>
+                </div>
             </div>
-        </div>
-    )
+        </React.Fragment>
+    );
 }
