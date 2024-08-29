22be2ea0244bf771bc59a6a85a95f61fe2fbd764
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-29 12:23:55 +0300

    описание типов ProfileType и передача данных в props

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -2,17 +2,38 @@ import React from "react";
 import {ProfileInfo} from "./profileInfo/ProfileInfo";
 import {MyPostsContainer} from "./myPosts/MyPostsContainer";
 
+export type ContactsType = {
+    facebook: string
+    github: string
+    instagram: string
+    mainLink: string
+    twitter: string
+    vk: string
+    website: null
+    youtube: null
+}
+export type PhotosType = {
+    large: string
+    small: string
+}
+export type ProfileType = {
+    aboutMe: string
+    contacts: ContactsType
+    fullName: string
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    photos: PhotosType
+}
+
+
 type ProfileProps = {
-    profile:{
-        photos:{
-            large:string
-        }
-    }
+    profile: ProfileType
 }
 export const Profile = (props: ProfileProps) => {
+    console.log(props.profile)
     return (
         <div>
-            <ProfileInfo profile={props.profile} />
+            <ProfileInfo profile={props.profile}/>
             <MyPostsContainer/>
         </div>
     )
