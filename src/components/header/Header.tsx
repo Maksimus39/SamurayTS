0bc8e2f131fdbf61330feef1cf01ced1049d7a68
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-30 23:53:13 +0300

    компонент Header принял props

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import classes from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-export const Header = () => {
+
+type HeaderProps = {
+    img:string
+}
+export const Header = (props: HeaderProps) => {
     return (
         <header className={classes.header}>
-            <img src="https://seousluga.ru/files/images/izobrazhenie_2024-02-07_143427682.png" alt=""/>
+            <img src={props.img} alt=""/>
+
+            <div className={classes.loginBlock}>
+                <NavLink to={'/login'}>
+                    Login
+                </NavLink>
+            </div>
         </header>
     )
 }
