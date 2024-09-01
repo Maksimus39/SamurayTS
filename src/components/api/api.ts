be0cc3d6ffad68e30daa0ff73b5714e86bfa0a09
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-01 21:17:47 +0300

    вынос функций в отдельную сущьность DAL

---
+++ src/components/api/api.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+type GetUsersProps = {
+    currentPage: number;
+    pageSize: number;
+}
+const apiKey = 'e6eaa2aa-df7a-4193-8c16-782484153fda';
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': apiKey
+    }
+})
+
+export const usersApi = {
+    getUsers({currentPage, pageSize}: GetUsersProps) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data);
+
+    },
+    getProfile(userId: number) {
+        return instance.get(`profile/${userId}`)
+            .then(response => response.data);
+    },
+    unfollowUser(userId: number) {
+        return instance.delete(`follow/${userId}`).then(response => response.data);
+    },
+    followUser(userId: number) {
+        return instance.post(`follow/${userId}`, {}).then(response => response.data);
+    },
+    getAuthUserData() {
+        return instance.get(`auth/me`).then(response => response.data);
+    }
+}
