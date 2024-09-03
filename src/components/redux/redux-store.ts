b11374ddb5ba9c4a3ae515a8cf2e66470be0f2ad
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-03 23:34:52 +0300

    в store добавили промежуточное звено thunkMiddleware

--- src/components/redux/redux-store.ts
+++ src/components/redux/redux-store.ts
@@ -1,9 +1,10 @@
-import {combineReducers, createStore, Store} from "redux";
+import {applyMiddleware, combineReducers, createStore, Store} from "redux";
 import {profilePageReducer} from "./reducer/profilePageReducer";
 import {dialogsPageReducer} from "./reducer/dialogsPageReducer";
 import {sidebarPageReducer} from "./reducer/SidePageReducer";
 import {usersPageReducer} from "./reducer/usersPageReducer";
 import {authReducer} from "./reducer/authPageReducer";
+import thunkMiddleware from "redux-thunk";
 
 
 let reducer = combineReducers({
@@ -16,7 +17,7 @@ let reducer = combineReducers({
 
 export type AppRootStateType = ReturnType<typeof reducer>
 
-export let store: Store<AppRootStateType> = createStore(reducer);
+export let store: Store<AppRootStateType> = createStore(reducer, applyMiddleware(thunkMiddleware));
 
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
