d554a1704d04c9ac25eee1255f909485fb7dbcef
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-04 07:44:58 +0300

    поменял синтаксис в App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from "./components/navbar/Navbar";
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import {News} from "./components/news/News";
 import {Music} from "./components/music/Music";
 import {Settings} from "./components/settings/Settings";
 import {DialogsContainer} from "./components/dialogs/DialogsContainer";
-import UsersContainer from './components/users/UsersContainer';
 import ProfileContainer from "./components/profile/ProfileContainer";
 import HeaderContainer from "./components/header/HeaderContainer";
-
+import UsersContainer from "./components/users/UsersContainer";
 
 function App() {
-
     return (
-
         <div className={'app-wrapper'}>
-
             <HeaderContainer/>
             <Navbar/>
-
             <div className={'app-wrapper-content'}>
-                <Route path={'/profile/:userId?'} render={() => <ProfileContainer/>}/>
-
-                <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
-
-                <Route path={'/users'} render={() => <UsersContainer/>}/>
-
-                <Route path={'/news'} component={News}/>
-                <Route path={'/music'} component={Music}/>
-                <Route path={'/settings'} component={Settings}/>
+                <Switch>
+                    <Route path={'/profile/:userId?'} render={() => <ProfileContainer/>}/>
+                    <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
+                    <Route path={'/users'} render={() => <UsersContainer/>}/>
+                    <Route path={'/news'} component={News}/>
+                    <Route path={'/music'} component={Music}/>
+                    <Route path={'/settings'} component={Settings}/>
+                </Switch>
             </div>
         </div>
     );
