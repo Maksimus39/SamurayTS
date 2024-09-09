67027371f7ffa5314aed8346e0bb2cb772e3d6ff
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-09 21:01:28 +0300

    Добавил классы стилей и новый компонент ProfileStatus

--- src/components/profile/profileInfo/ProfileInfo.tsx
+++ src/components/profile/profileInfo/ProfileInfo.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import {Preloader} from "../../common/preloader/Preloader";
 import {ContactsType, ProfileType} from "../Profile";
+import classes from './ProfileInfo.module.css'
+import {ProfileStatus} from "../ProfileStatus";
+
 
 type ProfileInfoProps = {
     profile: ProfileType;
 }
 
-const Contacts = ({ contacts }: { contacts: ContactsType }) => (
+const Contacts = ({contacts}: { contacts: ContactsType }) => (
     <div>
         <h3>Мои контакты!</h3>
         {Object.entries(contacts).map(([key, value]) => (
@@ -17,25 +20,29 @@ const Contacts = ({ contacts }: { contacts: ContactsType }) => (
 
 export const ProfileInfo = (props: ProfileInfoProps) => {
     if (!props.profile) {
-        return <Preloader />;
+        return <Preloader/>;
     }
 
-    const { profile } = props;
+    const {profile} = props;
 
     return (
         <React.Fragment>
-            <div>
+            <div className={classes.avatar}>
                 <img
                     src="https://avatars.dzeninfra.ru/get-zen_doc/271828/pub_655658c72ac2da7753ee753c_6556596de3bfcb307785efa6/scale_1200"
                     alt="Profile background"
                 />
+                <h4>status: <ProfileStatus status={'Hello my friends'}/></h4>
             </div>
 
             <div>
-                <img src={profile.photos.small} alt="Profile avatar"/>
+                <img src={profile.photos.small}
+                     alt="Profile avatar"
+                     className={classes.profileBackground}
+                />
                 <div>
                     <h2>Обо мне: {profile.aboutMe}</h2>
-                    <Contacts contacts={profile.contacts} />
+                    <Contacts contacts={profile.contacts}/>
                     <h2>Моё имя: {profile.fullName}</h2>
                     <h2>Место работы: {profile.lookingForAJob ? 'Ищу работу' : 'Не ищу работу'}</h2>
                     <h2>Вакансии: {profile.lookingForAJobDescription}</h2>
