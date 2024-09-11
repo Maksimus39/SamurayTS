efd0f67b1e833af1dc053a399e58c3f5f7467451
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-11 23:11:59 +0300

    componentDidUpdate

--- src/components/profile/ProfileStatus.tsx
+++ src/components/profile/ProfileStatus.tsx
@@ -32,6 +32,12 @@ export class ProfileStatus extends React.Component <ProfileStatusProps, ProfileS
         })
     }
 
+    componentDidUpdate(prevProps: Readonly<ProfileStatusProps>, prevState: Readonly<ProfileStatusState>) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({status: this.props.status})
+        }
+    }
+
     render() {
         return <>
             {!this.state.editMode &&
