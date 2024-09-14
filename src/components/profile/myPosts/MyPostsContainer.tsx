c3c778c765fb21970a953c7b74854669aa84aa8b
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-14 11:14:40 +0300

    Удаление лишних данных

--- src/components/profile/myPosts/MyPostsContainer.tsx
+++ src/components/profile/myPosts/MyPostsContainer.tsx
@@ -1,4 +1,4 @@
-import {addPost, updateNewPostText} from "../../redux/reducer/profilePageReducer";
+import {addPost} from "../../redux/reducer/profilePageReducer";
 import {MyPosts} from "./MyPosts";
 import {connect} from "react-redux";
 import {AppRootStateType} from "../../redux/redux-store";
@@ -6,10 +6,8 @@ import {PostDataType} from "../../redux/store";
 
 export type MapStatePropsType = {
     posts: PostDataType[]
-    newPostText: string
 }
 export type MapDispatchToPropsType = {
-    updateNewPostText: (text: string) => void
     addPost: (newPostText: string) => void
 }
 export type MyPostType = MapStatePropsType & MapDispatchToPropsType
@@ -18,12 +16,10 @@ export type MyPostType = MapStatePropsType & MapDispatchToPropsType
 const mapStateToProps = (state: AppRootStateType): MapStatePropsType => {
     return {
         posts: state.profilePage.posts,
-        newPostText: state.profilePage.newPostText,
     }
 }
 
 export const MyPostsContainer = connect(mapStateToProps, {
-    updateNewPostText,
     addPost
 })(MyPosts)
 
