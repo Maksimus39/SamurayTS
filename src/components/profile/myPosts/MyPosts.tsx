2cb47a062d25e10a95d519588536b020b0ba42c1
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-14 11:14:10 +0300

    Удалил неиспользуемые коллбеки, беру данные из reduxForm и создал новую компонент для этого

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -2,6 +2,7 @@ import React, {useRef} from 'react';
 import classes from "./MyPosts.module.css"
 import {Post} from "./posts/Post";
 import {MyPostType} from "./MyPostsContainer";
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 
 
 export const MyPosts = (props: MyPostType) => {
@@ -14,35 +15,40 @@ export const MyPosts = (props: MyPostType) => {
 
     const newPostElement = useRef<HTMLTextAreaElement>(null);
 
-    const onAddPost = () => {
-        if (newPostElement.current) {
-            const text = newPostElement.current.value;
-            props.addPost(text)
-        }
-
+    const onAddPost = (values: OnnAddPostValuesType) => {
+        // if (newPostElement.current) {
+        //     const text = newPostElement.current.value;
+        //     props.addPost(text)
+        // }
+        props.addPost(values.newPostText)
     };
 
-    const onPostChange = () => {
-        if (newPostElement.current) {
-            const text = newPostElement.current.value;
-            if (text.trim() !== "") {
-                props.updateNewPostText(text)
-            }
-        }
-    }
+    // const onPostChange = () => {
+    //     if (newPostElement.current) {
+    //         const text = newPostElement.current.value;
+    //         if (text.trim() !== "") {
+    //             props.updateNewPostText(text)
+    //         }
+    //     }
+    // }
 
     return (
         <div className={classes.postsBlock}>
             <h3>My Posts</h3>
-            <div>
-                <div>
-                    <textarea ref={newPostElement} onChange={onPostChange} value={props.newPostText}
-                              placeholder={'Enter Your Post'}/>
-                </div>
-                <div>
-                    <button onClick={onAddPost}>Add Post</button>
-                </div>
-            </div>
+            {/*<form>*/}
+            {/*    <div>*/}
+            {/*        <textarea ref={newPostElement}*/}
+            {/*                  onChange={onPostChange}*/}
+            {/*                  value={props.newPostText}*/}
+            {/*                  placeholder={'Enter Your Post'}*/}
+            {/*        />*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <button onClick={onAddPost}>Add Post</button>*/}
+            {/*    </div>*/}
+            {/*</form>*/}
+
+            <AddNewPostFormRedux onSubmit={onAddPost}/>
 
             <div className={classes.posts}>
 
@@ -51,4 +57,31 @@ export const MyPosts = (props: MyPostType) => {
         </div>
     );
 };
+
+
+// Интерфейс для значений формы
+interface OnnAddPostValuesType {
+    newPostText: string;
+}
+type AddNewPostFormProps =InjectedFormProps<OnnAddPostValuesType>;
+
+const AddNewPostForm = (props: AddNewPostFormProps) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+
+            <div>
+                <Field name={'newPostText'}
+                       component={'textarea'}
+                       placeholder={'Enter a message...'}/>
+            </div>
+
+            <div>
+                <button>Add Post</button>
+            </div>
+
+        </form>
+    )
+}
+
+let AddNewPostFormRedux = reduxForm<OnnAddPostValuesType>({form: 'ProfileAddNewPostForm'})(AddNewPostForm)
 
