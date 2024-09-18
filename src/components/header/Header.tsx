8b99f49884931ecd0b55c7603eca44dd36843dbc
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-18 15:09:56 +0300

    Доработка логики для вылогинизации

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,27 +4,31 @@ import {NavLink} from "react-router-dom";
 
 
 type HeaderProps = {
-    isAuth: boolean
-    email:any
-    login: any
+    isAuth: boolean,
+    email: string,
+    login: string,
+    logout: () => void
 }
 export const Header = (props: HeaderProps) => {
 
     return (
         <header className={classes.header}>
-            <img src='https://avatars.mds.yandex.net/i?id=7a3f7ada082cecc5d96c66033cf2bff22b65750c-10126262-images-thumbs&n=13' alt=""/>
+            <img
+                src='https://avatars.mds.yandex.net/i?id=7a3f7ada082cecc5d96c66033cf2bff22b65750c-10126262-images-thumbs&n=13'
+                alt=""/>
 
             <div className={classes.loginBlock}>
 
-                {props.isAuth ?
-
-                    <div>
-                        <div>{props.login}</div>
+                {props.isAuth
+                    ? <div>
+                        <div>{props.login}
+                            <button onClick={props.logout}>Log out</button>
+                        </div>
                         <div>{props.email}</div>
                     </div>
 
                     : <NavLink to={'/login'}>
-                    Login
+                        Login
                     </NavLink>
                 }
             </div>
