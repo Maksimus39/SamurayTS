5fb037f6d094ab8e685856dc71fcce511cdf88f0
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-28 01:11:05 +0300

    Рефакторинг компонент Users

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,18 +1,17 @@
-import styles from "./users.module.css";
-import userPhoto from "../assets/images/user.png";
 import React from "react";
-import {UsersDataType} from "../redux/store";
-import {NavLink} from "react-router-dom";
+import { UsersDataType } from "../redux/store";
+import { Paginator } from "../common/paginator/Paginators";
+import { User } from "./User";
 
 type UsersProps = {
-    totalUsersCount: number
-    pageSize: number
-    currentPage: number
-    onPageChanged: (pageNumber: number) => void
-    users: UsersDataType[]
-    unfollow: (userId: number) => void
-    follow: (userId: number) => void
-    followingInProgress: boolean
+    totalUsersCount: number;
+    pageSize: number;
+    currentPage: number;
+    onPageChanged: (pageNumber: number) => void;
+    users: UsersDataType[];
+    unfollow: (userId: number) => void;
+    follow: (userId: number) => void;
+    followingInProgress: boolean;
 }
 
 export const Users = ({
@@ -26,61 +25,24 @@ export const Users = ({
                           followingInProgress,
                       }: UsersProps) => {
 
-    let pagesCount = Math.ceil(totalUsersCount / pageSize)
-    let pages = []
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
-    }
-
-    return <div>
+    return (
         <div>
-            {pages.map((p) => (
-                <span key={p} className={currentPage === p ? styles.selectedPage : ''}
-                      onClick={() => {
-                          onPageChanged(p)
-                      }}>
-                    {p}
-                </span>
-            ))}
-        </div>
-        <div>
-            {users.map(us => (
-                <div key={us.id}>
-                    <span>
-                        <div>
-                            <NavLink to={'/profile/' + us.id}>
-                                <img src={us.photos.small != null
-                                    ? us.photos.small
-                                    : userPhoto} alt={us.name}
-                                     className={styles.userPhoto}/>
-                            </NavLink>
-                        </div>
-                        <div>
-                            {us.followed
-                                ? <button disabled={followingInProgress}
-                                          className={styles.button}
-                                          onClick={() => {
-                                              unfollow(us.id)
-                                          }}>Unfollow</button>
+            <Paginator totalUsersCount={totalUsersCount}
+                       pageSize={pageSize}
+                       currentPage={currentPage}
+                       onPageChanged={onPageChanged} />
 
-                                : <button disabled={followingInProgress}
-                                          className={styles.button}
-                                          onClick={() => {
-                                              follow(us.id)
-                                          }}>Follow</button>
-                            }
-                        </div>
-                    </span>
-                    <span>
-                        <div>{us.name}</div>
-                        <div>{us.status}</div>
-                    </span>
-                    <span>
-                        <div>{'us.location.country'}</div>
-                        <div>{'us.location.city'}</div>
-                    </span>
-                </div>
-            ))}
+            <div>
+                {users.map(user => (
+                    <User
+                        key={user.id}
+                        user={user}
+                        unfollow={unfollow}
+                        follow={follow}
+                        followingInProgress={followingInProgress}
+                    />
+                ))}
+            </div>
         </div>
-    </div>
+    );
 }
