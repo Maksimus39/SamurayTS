7a1c6a7db54d19439a18853dc29fb9719649e152
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-28 01:07:07 +0300

    Деструктурировал props

--- src/components/dialogs/dialogItem/DialogItem.tsx
+++ src/components/dialogs/dialogItem/DialogItem.tsx
@@ -8,16 +8,16 @@ type DialogPropsItem = {
     id: number
     name: string
 }
-export const DialogItem = (props: DialogPropsItem) => {
+export const DialogItem = ({img,id,name}: DialogPropsItem) => {
     return (
         <div>
             <div className={classes.dialog + ' ' + classes.active}>
-                <NavLink to={'/dialogs/1' + props.id}>
-                    {props.name}</NavLink>
+                <NavLink to={'/dialogs/1' + id}>
+                    {name}</NavLink>
             </div>
 
             <div className={classes.avatar}>
-                <img src={props.img} alt=""/>
+                <img src={img} alt=""/>
             </div>
         </div>
     )
