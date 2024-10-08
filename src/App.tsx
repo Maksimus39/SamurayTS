bbfff36ef1230d7e31e278363c1f87782db48e9c
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-10-08 11:25:27 +0300

    12. Уроки React JS (Разбивка на компоненты)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {Header} from "./Header";
-import {Navbar} from "./Navbar";
-import {Profile} from "./Profile";
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 import './App.css'
 
 export const App = () => {
