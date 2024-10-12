aae083011508ef6c6843c409064545a2e8921426
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-10-12 21:53:03 +0300

    20. Уроки React JS (NavLink) - react курсы бесплатно

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import styles from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 
 export const Navbar = () => {
     return (
         <div className={styles.navbar}>
-            <div><a href="/profile">Profile</a></div>
-            <div><a href="/dialogs">Messages</a></div>
-            <div><a href="/news">News</a></div>
-            <div><a href="/music">Music</a></div>
-            <div><a href="/settings">Settings</a></div>
+            <div>
+                <NavLink to="/profile">Profile</NavLink>
+            </div>
+            <div>
+                <NavLink to="/dialogs">Messages</NavLink>
+            </div>
+            <div>
+                <NavLink to="/news">News</NavLink>
+            </div>
+            <div>
+                <NavLink to="/music">Music</NavLink>
+            </div>
+            <div>
+                <NavLink to="/settings">Settings</NavLink>
+            </div>
         </div>
     )
 }
